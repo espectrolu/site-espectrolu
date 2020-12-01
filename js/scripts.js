66e3143fb473172898aceb49fb35f6f6570321d8
Guilherme Abreu Carvalho <68426772+guilherme-abreu@users.noreply.github.com>
2020-11-30 21:57:49 -0300

    Alterando combos

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,21 @@ function mudarCorContatos() {
   $(facebook).animate({opacity: 0.5},500);
   $(facebook).animate({opacity: 1.0},500);
   $(facebook).animate({opacity: 0.5},500);
+  $(facebook).animate({opacity: 1.0},500);  
+  $(facebook).animate({opacity: 0.5},500);
+  $(facebook).animate({opacity: 1.0},500);
+  $(facebook).animate({opacity: 0.5},500);
+  $(facebook).animate({opacity: 1.0},500);
+  $(facebook).animate({opacity: 0.5},500);
   $(facebook).animate({opacity: 1.0},500);
 
   whatsapp = document.getElementById("whatsapp");
+  $(whatsapp).animate({opacity: 0.5},500);
+  $(whatsapp).animate({opacity: 1.0},500);
+  $(whatsapp).animate({opacity: 0.5},500);
+  $(whatsapp).animate({opacity: 1.0},500);
+  $(whatsapp).animate({opacity: 0.5},500);
+  $(whatsapp).animate({opacity: 1.0},500);
   $(whatsapp).animate({opacity: 0.5},500);
   $(whatsapp).animate({opacity: 1.0},500);
   $(whatsapp).animate({opacity: 0.5},500);
@@ -31,8 +43,20 @@ function mudarCorContatos() {
   $(instagram).animate({opacity: 1.0},500);
   $(instagram).animate({opacity: 0.5},500);
   $(instagram).animate({opacity: 1.0},500);
+  $(instagram).animate({opacity: 0.5},500);
+  $(instagram).animate({opacity: 1.0},500);
+  $(instagram).animate({opacity: 0.5},500);
+  $(instagram).animate({opacity: 1.0},500);
+  $(instagram).animate({opacity: 0.5},500);
+  $(instagram).animate({opacity: 1.0},500);
 
   envelope = document.getElementById("envelope");
+  $(envelope).animate({opacity: 0.5},500);
+  $(envelope).animate({opacity: 1.0},500);
+  $(envelope).animate({opacity: 0.5},500);
+  $(envelope).animate({opacity: 1.0},500);
+  $(envelope).animate({opacity: 0.5},500);
+  $(envelope).animate({opacity: 1.0},500);
   $(envelope).animate({opacity: 0.5},500);
   $(envelope).animate({opacity: 1.0},500);
   $(envelope).animate({opacity: 0.5},500);
